69478c5bccd88032f7d067c97d4ae7c36a9fc619
Zahraa222 <zzmmk2003@gmail.com>
2024-03-20 12:50:48 -0400

    Main Layout

--- src/Book.java
+++ src/Book.java
@@ -2,11 +2,18 @@ import javax.swing.*;
 
 public class Book {
     private JPanel main;
-    private JTabbedPane tabbedPane1;
+    private JTabbedPane tabs;
+    private JPanel Favorite;
+    private JPanel Recent;
+    private JPanel Contacts;
+    private JPanel Settings;
 
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame();
+        frame.setContentPane(new Book().main);
+        frame.setBounds(200,200,500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setVisible(true);
     }
-
-
 }

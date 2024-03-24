e3a746124a5d612ab33e1935607d00449fffa157
Zahraa222 <zzmmk2003@gmail.com>
2024-03-24 17:38:55 -0400

    minor edits

--- src/Book.java
+++ src/Book.java
@@ -5,9 +5,8 @@ import java.awt.event.ActionListener;
 public class Book {
     private JPanel main;
     private JTabbedPane tabs;
-    private JButton editButton;
     private JButton createNewContactButton;
-    private JList ContactList;
+
     private JButton button12;
     private JButton a7Button;
     private JButton a8Button;
@@ -21,6 +20,9 @@ public class Book {
     private JButton a0Button;
     private JTextField Screen;
     private JButton Clear;
+    private JButton call;
+    private JSlider slider1;
+    private JButton editButton;
     private JPanel Favorite;
     private JPanel Recent;
     private JPanel Contacts;
@@ -28,64 +30,75 @@ public class Book {
 
 
     public Book(){
+        checkphoneLength();
         a0Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Screen.setText(Screen.getText() + a0Button.getText());
+                checkphoneLength();
             }
         });
         a1Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Screen.setText(Screen.getText() + a1Button.getText());
+                checkphoneLength();
             }
         });
         a2Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Screen.setText(Screen.getText() + a2Button.getText());
+                checkphoneLength();
             }
         });
         a3Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Screen.setText(Screen.getText() + a3Button.getText());
+                checkphoneLength();
             }
         });
         a4Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Screen.setText(Screen.getText() + a4Button.getText());
+                checkphoneLength();
             }
         });
         a5Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Screen.setText(Screen.getText() + a5Button.getText());
+                checkphoneLength();
             }
         });
         a6Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Screen.setText(Screen.getText() + a6Button.getText());
+                checkphoneLength();
             }
         });
         a7Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Screen.setText(Screen.getText() + a7Button.getText());
+                checkphoneLength();
             }
         });
         a8Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Screen.setText(Screen.getText() + a8Button.getText());
+                checkphoneLength();
             }
         });
         a9Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Screen.setText(Screen.getText() + a9Button.getText());
+                checkphoneLength();
             }
         });
         Clear.addActionListener(new ActionListener() {
@@ -96,13 +109,41 @@ public class Book {
         });
 
 
+
         JFrame frame = new JFrame();
         frame.setContentPane(main);
         frame.setBounds(200,200,500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(360,500);
+        frame.setSize(370,500);
         frame.setVisible(true);
+        call.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String phoneNumber = Screen.getText();
+                JCheckBox checkBox = new JCheckBox(phoneNumber);
+                Recent.add(checkBox);
+                tabs.revalidate();
+                tabs.repaint();
+            }
+        });
     }
+
+    private void checkphoneLength() {
+        if(Screen.getText().length() > 13){
+            JOptionPane.showMessageDialog(null, "Phone Number Must be 10 characters long", "Invalid Format", JOptionPane.ERROR_MESSAGE);
+            Screen.setText("");
+        }
+        if(Screen.getText().length() == 4){
+            Screen.setText(Screen.getText() + ')');
+        }
+        if(Screen.getText().length() == 8){
+            Screen.setText(Screen.getText() + '-');
+        }
+        if(Screen.getText().isEmpty()){
+            Screen.setText("(");
+        }
+    }
+
     public static void main(String[] args) {
         new Book();
     }
